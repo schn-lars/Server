ea4ba1428b0d983ff45c83f85d3ca0d1ad4125fb
Lars Schneider <lars.schneider@stud.unibas.ch>
2026-06-06 13:54:23 +0200

    Add print statement to check that load_model has been successfully called on the backend.

--- api/service/inference.py
+++ api/service/inference.py
@@ -48,6 +48,7 @@ class InferenceSession:
         @param task:      [Segmentation, Detection] 
     '''
     def load_model(self, model_type: str, task: str):
+        print("load_model:", model_type)
         if model_type == "YOLOv26":
             self.yolo_model = YOLO("yolo26s-seg.pt" if task == 'Segmentation' else 'yolo26s.pt')
             self.yolo_model.to(DEVICE)
